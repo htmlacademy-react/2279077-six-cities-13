7d0fe7050c32b4565582eafea393f97a4a6f4bc5
Diana <arazklycheva94@mail.ru>
2023-07-25 16:49:41 +0500

    embed the gallery in the detailed offer card

--- src/app.tsx
+++ src/app.tsx
@@ -3,18 +3,19 @@ import { HelmetProvider } from 'react-helmet-async';
 import { MainPage } from './pages/main/main-page';
 import FavoritesPage from './pages/favorites-page/favorites-page';
 import { LoginPage } from './pages/login/login-page';
-import { OfferPage } from './pages/offer/offer-page';
+import OfferPage from './pages/offer/offer-page';
 import { NotFoundPage } from './pages/not-found-page/not-found-page';
 import { PrivateRoute } from './components/private-route/private-route';
 import { AppRoute, AuthorizationStatus } from './const';
-import { Offer } from './types/offer';
+import { Offer, OfferDetail } from './types/offer';
 
 type AppScreenProps = {
   offers: Offer[];
   favoritesOffers: Offer[];
+  detailsOffers: OfferDetail[];
 }
 
-export const App = ({offers, favoritesOffers}: AppScreenProps): JSX.Element => (
+export const App = ({offers, favoritesOffers, detailsOffers}: AppScreenProps): JSX.Element => (
   <HelmetProvider>
     <BrowserRouter>
       <Routes>
@@ -40,8 +41,8 @@ export const App = ({offers, favoritesOffers}: AppScreenProps): JSX.Element => (
             </PrivateRoute>
           }
         />
-        <Route path={AppRoute.Offer} element={<OfferPage />}>
-          <Route path=':id' element={<OfferPage />}/>
+        <Route path={AppRoute.Offer} element={<OfferPage detailsOffers={detailsOffers}/>}>
+          <Route path=':id' element={<OfferPage detailsOffers={detailsOffers}/>}/>
         </Route>
         <Route
           path='*'
