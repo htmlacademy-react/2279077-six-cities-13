7b7d72a5370b4ada4281b6db7fb1e6dfb0b2d85f
Diana <arazklycheva94@mail.ru>
2023-08-21 16:12:09 +0500

    add support for tokens

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,25 @@
 import axios from 'axios';
-import { AxiosInstance } from 'axios';
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { BACKEND_URL, REQUEST_TIMEOUT } from '../const';
+import { getToken } from './token';
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
 
   return api;
 };
