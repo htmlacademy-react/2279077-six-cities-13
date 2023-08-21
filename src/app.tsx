55fa0603b15efcca8de79e1c65a8524d35dd1ca1
Diana <arazklycheva94@mail.ru>
2023-08-21 16:52:45 +0500

    add form handling to login-page

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,15 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import MainPage from './pages/main/main-page';
 import FavoritesPage from './pages/favorites-page/favorites-page';
-import { LoginPage } from './pages/login/login-page';
+import LoginPage from './pages/login/login-page';
 import OfferPage from './pages/offer/offer-page';
 import { NotFoundPage } from './pages/not-found-page/not-found-page';
 import { PrivateRoute } from './components/private-route/private-route';
-import { AppRoute, AuthorizationStatus } from './const';
+import { AppRoute } from './const';
+import { useAppSelector } from './components/hooks';
 
 function App(): JSX.Element {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
   return(
     <HelmetProvider>
       <BrowserRouter>
@@ -27,7 +29,7 @@ function App(): JSX.Element {
             path={AppRoute.Favorites}
             element={
               <PrivateRoute
-                authorizationStatus={AuthorizationStatus.NoAuth}
+                authorizationStatus={authorizationStatus}
               >
                 <FavoritesPage />
               </PrivateRoute>
